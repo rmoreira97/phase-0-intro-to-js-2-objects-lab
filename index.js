9235129d4d4a17baef310b9f307763c16776f9e7
rafael moreira <rafa.siffert@gmail.com>
2023-08-02 21:02:07 -0400

    Objects

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+
+let employee = {
+  name: 'Sam',
+};
+
+// Function to update an employee with a new key-value pair, returning a new object without modifying the original.
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign({}, employee, { [key]: value });
+}
+
+// Function to destructively update an employee with a new key-value pair and return the updated employee object.
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Function to delete a specific key from the employee object, returning a new object without the deleted key-value pair.
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = Object.assign({}, employee);
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Function to destructively delete a specific key from the employee object and return the modified employee object.
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
